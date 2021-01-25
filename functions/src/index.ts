00f7df9e976d6c0e4f98a923ad7b929bd6aefa81
Ville Takanen <vitkukissa@gmail.com>
2021-01-25 15:51:05 +0200

    feat: added count updating

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,5 +1,6 @@
 import * as functions from 'firebase-functions';
 import algoliasearch from 'algoliasearch'
+import admin = require('firebase-admin');
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
@@ -16,6 +17,9 @@ const ALGOLIA_ADMIN_KEY = functions.config().algolia.api_key
 const ALGOLIA_INDEX_NAME = 'pelilauta_discussion';
 const client = algoliasearch(ALGOLIA_ID, ALGOLIA_ADMIN_KEY)
 
+admin.initializeApp()
+const db = admin.firestore()
+
 // Update the search index every time a thread post is written.
 export const onThreadCreated = functions.firestore.document('stream/{threadId}').onCreate((snap, context) => {
   // Get the note document
@@ -31,7 +35,23 @@ export const onThreadCreated = functions.firestore.document('stream/{threadId}')
   
   if (!index) throw new Error('could not init index ' + ALGOLIA_INDEX_NAME)
 
-  return index.saveObject(note)
+  return index.saveObject(note).then(() => {
+    return db.collection('meta').doc('pelilauta').get().then((metadoc) => {
+      const streams = metadoc.data()?.streams
+      streams[note.topic].count = streams[note.topic].count + 1
+      return db.collection('meta').doc('pelilauta').update({streams: streams})
+    })
+  })
+})
+
+export const onThreadDeleted = functions.firestore.document('stream/{threadId}').onDelete((snap, context) => {
+  // Get the note document
+  const note = snap.data()
+  return db.collection('meta').doc('pelilauta').get().then((metadoc) => {
+    const streams = metadoc.data()?.streams
+    streams[note.topic].count = streams[note.topic].count - 1
+    return db.collection('meta').doc('pelilauta').update({streams: streams})
+  })
 })
 
 // Update the search index every time a thread post is written.
@@ -51,6 +71,10 @@ export const onThreadUpdated = functions.firestore.document('stream/{threadId}')
 
   return index.saveObject(note)
 })
+
+
+
+
 
 /* export const threadKudos = functions.firestore.document('stream/{docID}')
   .onUpdate((change, context) => { 
