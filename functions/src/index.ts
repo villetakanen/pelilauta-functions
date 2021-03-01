89950a43b021315efb8adc6dc3fcbacb5b203293
Ville Takanen <vitkukissa@gmail.com>
2021-03-01 15:54:12 +0200

    fix: correct recent changes field

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -103,7 +103,7 @@ export const onWikiPageUpdated = functions.firestore.document('sites/{siteid}/pa
     name: update?.name || pageid,
     content: update?.htmlContent || '- no content -',
     author: update?.author || '- unknown author id -',
-    changetime: update?.created || null
+    changetime: update?.lastUpdate || null
   })
 })
 
